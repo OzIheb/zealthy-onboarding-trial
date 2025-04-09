63838198c646cba6ea9255e5a19400fca8c808ed
OzIheb <ouelhazi3iheb@gmail.com>
2025-04-09 02:06:51 +0100

    test different next configs

--- next.config.ts
+++ next.config.ts
@@ -4,13 +4,6 @@ import { PrismaPlugin } from '@prisma/nextjs-monorepo-workaround-plugin';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.plugins = [...config.plugins, new PrismaPlugin()];
-    }
-
-    return config;
-  },
 };
 
 export default nextConfig;
