51a416adb7b16f7f5dda4f83c98945ed22a72794
OzIheb <ouelhazi3iheb@gmail.com>
2025-04-09 11:57:51 +0100

    add postinstall script

--- next.config.ts
+++ next.config.ts
@@ -4,13 +4,6 @@ import { PrismaPlugin } from '@prisma/nextjs-monorepo-workaround-plugin';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  webpack: (config: { plugins: any[]; }, { isServer }: any) => {
-    if (isServer) {
-      config.plugins = [...config.plugins, new PrismaPlugin()];
-    }
-
-    return config;
-  },
 };
 
 export default nextConfig;
